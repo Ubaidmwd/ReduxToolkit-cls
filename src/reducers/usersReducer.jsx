299172be5e80e1106a710a9fdb5aa26f22996c94
Ubaidmwd <111492421+Ubaidmwd@users.noreply.github.com>
2023-01-16 23:21:46 -0800

    builder

--- src/reducers/usersReducer.jsx
+++ src/reducers/usersReducer.jsx
@@ -1,14 +1,22 @@
+import { createReducer } from "@reduxjs/toolkit"
 const initialstate={
     name:"ubaid",
     age:"24",
     status:"coder"
 }
 
-export const userData = (state= initialstate, action) => {
-  if(action.type=='UPDATE'){
-    return {
-        age:action.payload}
+// export const userData = (state= initialstate, action) => {
+//   if(action.type=='UPDATE'){
+//     return {
+//         ...state,
+//         age:action.payload}
 
-  }
-  return state;
-}
+//   }
+//   return state;
+// }
+
+export const userData= createReducer(initialstate,(builder)=>{
+    builder.addCase('UPDATE',(state,action)=>{
+        state.age=action.payload
+    })
+})
