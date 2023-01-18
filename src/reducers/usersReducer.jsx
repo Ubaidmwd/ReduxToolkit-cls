04b8b8a01f121fe599d2e41eb538736017fdc867
Ubaidmwd <111492421+Ubaidmwd@users.noreply.github.com>
2023-01-17 23:10:57 -0800

    pending & rejected

--- src/reducers/usersReducer.jsx
+++ src/reducers/usersReducer.jsx
@@ -1,38 +1,47 @@
-import { createSlice ,createAsyncThunk} from "@reduxjs/toolkit"
-const initialState={
-    userName:"ubaid",
-    age:"24",
-    status:"coder"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+const initialState = {
+    userName: "ubaid",
+    age: "24",
+    status: "coder"
 }
 export const fetchUsersName = createAsyncThunk(
     'fetchUsers',
     async () => {
-        const res=await fetch('https://jsonplaceholder.typicode.com/users')
-        const res2=await res.json()
-    //   const response = await userAPI.fetchById(userId)
-    //   return response.data
-        return res2[0].name
+        const res = await fetch('https://jsonplaceholder.typicode.com/users')
+        const res2 = await res.json()
+        //   const response = await userAPI.fetchById(userId)
+        //   return response.data
+        return res2[Math.floor(Math.random()*10)].name
     })
-  
-const userData=createSlice({
-    name:"users",
+
+const userData = createSlice({
+    name: "users",
     initialState,
-    reducers:{
-        updateName(state,action){
-            state.userName=action.payload
+    reducers: {
+        updateName(state, action) {
+            state.userName = action.payload
         },
-        updateAge(state,action){
-            state.age=action.payload
+        updateAge(state, action) {
+            state.age = action.payload
         }
     },
-    extraReducers:{
+    extraReducers: {
 
-    [fetchUsersName.fulfilled]:(state, action) => {
-        
-        state.userName=action.payload
-      }
+        [fetchUsersName.fulfilled]: (state, action) => {
+
+            state.userName = action.payload
+        },
+        [fetchUsersName.pending]: (state, action) => {
+
+            state.userName = '!!!!Loding!!!!'
+        }
+        ,
+        [fetchUsersName.rejected]: (state, action) => {
+
+            state.userName = '!!!!Try Again !!!!'
+        }
     }
 })
 
-export const { updateName,updateAge} = userData.actions
+export const { updateName, updateAge } = userData.actions
 export default userData.reducer
